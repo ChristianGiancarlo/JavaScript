c5f643a4ec971193ce56f7ca81b8a069d92b1abf
Christian Mimbela <christiangiancarl@gmail.com>
2023-02-27 20:15:37 -0300

    trabajo 2

---
+++ Proyecto2/prueba.js
@@ -0,0 +1,11 @@
+const libro = {
+    titulo: "Don Quijote de la mancha",
+    autor: "Miguel Cervantes",
+    fecha: new Date(1605,0,1),
+    url: "https://www.cervantesvirtual.com/obra-visor/el-ingenioso-hidalgo-don-quijote-de-la-mancha-6/html/05f86699-4b53-4d9b-8ab8-b40ab63fb0b3_2.html",
+}
+
+
+const datos = [ "Christian", 22, true, new Date(2000, 4, 6), libro]
+
+console.log(datos);
