6e6e76ccb7e0993d603df81abb7db2a2aacfcca0
Christian Mimbela <christiangiancarl@gmail.com>
2023-03-01 16:12:01 -0300

    Trabajo number

---
+++ Proyecto5/trabajoNumber.js
@@ -0,0 +1,12 @@
+let alturaCentimetros = 163;
+let alturaMetros = 1.63;
+let pesoKilogramo  = 61.5;
+
+let alturaRedondeada = Math.round(alturaMetros);
+console.log(alturaRedondeada);
+
+let pesoRedondeado = Math.floor(pesoKilogramo);
+console.log(pesoRedondeado);
+
+let comparacionNumero = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(comparacionNumero);
