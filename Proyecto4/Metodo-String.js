c9ccde5f785381e2748766e07a2a5815f33e1d67
Christian Mimbela <christiangiancarl@gmail.com>
2023-03-01 14:52:04 -0300

    trabajo de string

---
+++ Proyecto4/Metodo-String.js
@@ -0,0 +1,15 @@
+let nombre = 'Christian';
+let apellido = 'Mimbela';
+
+let estudiante = nombre.concat(" ",apellido);
+let estudianteMayus = estudiante.toUpperCase;
+let estudianteMinus = estudiante.toLowerCase;
+
+let numeroLetrasEstudiante = estudiante.length;
+
+let primeraLetraNombre = nombre.charAt(0);
+let ultimaLetraApellido =  apellido.charAt(9);
+
+let sinEspacios = estudiante.trim();
+
+let bool = estudiante.includes(nombre);
